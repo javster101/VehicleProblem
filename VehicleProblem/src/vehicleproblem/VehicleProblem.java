3a615ceb0cd50f52ec0ec69efa40daaa5679b2bd
Javier <Javier@SurfaceJavi>
2016-02-24 20:58:53 -0500

    Initial Commit

---
+++ VehicleProblem/src/vehicleproblem/VehicleProblem.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vehicleproblem;
+
+/**
+ *
+ * @author Javier
+ */
+public class VehicleProblem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
