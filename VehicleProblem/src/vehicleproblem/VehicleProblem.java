4d09109ab882b0aac890b8aa520586c6f4450ec1
Javier <Javier@SurfaceJavi>
2016-03-03 13:54:42 -0500

    Window

--- VehicleProblem/src/vehicleproblem/VehicleProblem.java
+++ VehicleProblem/src/vehicleproblem/VehicleProblem.java
@@ -6,17 +6,97 @@
 
 package vehicleproblem;
 
+import com.opengg.core.io.input.KeyboardEventHandler;
+import com.opengg.core.io.input.KeyboardListener;
+import com.opengg.core.movement.MovementLoader;
+import com.opengg.core.render.VertexArrayObject;
+import com.opengg.core.render.VertexBufferObject;
+import com.opengg.core.render.shader.ShaderController;
+import com.opengg.core.render.texture.Texture;
+import com.opengg.core.render.window.DisplayMode;
+import com.opengg.core.render.window.GLFWWindow;
+import static com.opengg.core.render.window.RenderUtil.endFrame;
+import static com.opengg.core.render.window.RenderUtil.startFrame;
+import com.opengg.core.util.GlobalInfo;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
+
 /**
  *
  * @author Javier
  */
-public class VehicleProblem {
-
+public class VehicleProblem implements KeyboardListener{
+    GLFWWindow w;
+    VertexBufferObject vbo;
+    VertexArrayObject vao;
+    Texture ground = new Texture();
+    ShaderController s = new ShaderController();
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new VehicleProblem();
+        
+    }
+    
+    public VehicleProblem(){
+        KeyboardEventHandler.addToPool(this);
+        try {
+            w = new GLFWWindow(1280, 960, "Vehicle Problem!", DisplayMode.WINDOWED);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        while (!w.shouldClose()) {
+
+            startFrame();
+
+            update();
+            render();
+            endFrame(w);
+        }
+        
+        exit();
+    }
+    
+    public void setup() throws UnsupportedEncodingException{
+        MovementLoader.setup(w.getID(), 80);
+
+        vao = new VertexArrayObject();
+        vao.bind();
+        vbo = new VertexBufferObject();
+        vbo.bind(GL_ARRAY_BUFFER);
+        GlobalInfo.b = vbo;
+        
+        ground.loadTexture("C:/res/ground.png", true);
+        
+        URL verts = VehicleProblem.class.getResource("res/shaders/shader.vert");
+        URL frags = VehicleProblem.class.getResource("res/shaders/shader.frag");
+        URL geoms = VehicleProblem.class.getResource("res/shaders/shader.geom");
+        
+         s.setup(verts, frags, geoms);
+    }
+    
+    public void render(){
+        
+    }
+    
+    public void update(){
+        
+    }
+    
+    public void exit(){
+        
+    }
+    
+    @Override
+    public void keyPressed(int key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyReleased(int key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
